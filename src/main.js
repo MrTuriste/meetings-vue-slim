37f7b6f5b426aca4c8f1e947ffa32ffb7ea70cf1
MrTuriste <43986311+MrTuriste@users.noreply.github.com>
2018-12-11 10:36:25 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Meetings from './Meetings.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import VueResource from "vue-resource";
 
+Vue.use(VueResource);
 new Vue({
   render: h => h(Meetings),
 }).$mount('#app');
