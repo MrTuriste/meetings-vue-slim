c5cc0af113081c552bc9b53d1605c06b9b88d6c6
MrTuriste <43986311+MrTuriste@users.noreply.github.com>
2018-12-11 10:36:51 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Meetings from './Meetings.vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import VueResource from "vue-resource";
+Vue.http.options.root = '/api';
 
 Vue.use(VueResource);
 new Vue({
